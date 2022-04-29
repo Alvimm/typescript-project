1aa157e4d7d4158c9d5fe734eda6df5df367d7f1
Alvimm <filipe2012alvim@gmail.com>
2022-04-29 16:53:02 -0300

    Fix gitignore

--- app/dist/js/interfaces/printable.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=printable.js.map
--- app/dist/js/utils/printOut.js
+++
@@ -1,6 +0,0 @@
-export function printOut(...objects) {
-    for (let object of objects) {
-        console.log(object.forText());
-    }
-}
-//# sourceMappingURL=printOut.js.map
